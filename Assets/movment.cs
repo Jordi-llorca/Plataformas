672fbaedf62efab2cb22d84aa295bec97bb5c968
Jordi <jorllorber@gmail.com>
2020-11-09 15:35:41 +0100

    Prueba cilindro

---
+++ Assets/movment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment : MonoBehaviour
+{
+    public float movSpeed = 600f;
+    float movement = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, movement * Time.deltaTime * movSpeed);
+    }
+}
