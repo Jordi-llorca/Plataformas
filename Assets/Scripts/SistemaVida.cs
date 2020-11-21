680238030e2bb925012d930271061ef6b3a0d5ae
KaiPhel <74152155+KaiPhel@users.noreply.github.com>
2020-11-21 21:06:59 +0100

    Sistema de vida y daño

---
+++ Assets/Scripts/SistemaVida.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float maxVida = 100.0f;
+    float actualVida;
+
+    public bool inmortal = false;
+    public float tiempoInmortal = 1.0f;
+
+    private void Start()
+    {
+       
+        actualVida = maxVida;
+
+    }
+
+    private void Update()
+    {
+        if (actualVida > maxVida)
+            actualVida = maxVida;
+
+        if (actualVida <= 0)
+            Muerte();
+    }
+
+    public void QuitarVida(float daño)
+    {
+        if (inmortal) return;
+        
+        actualVida -= daño;
+        StartCoroutine(TiempoInmortal());
+    }
+
+    public void DarVida(float vida)
+    {
+        actualVida += vida;
+    }
+
+    public void Muerte()
+    {
+        Destroy(this.gameObject); 
+    }
+
+    IEnumerator TiempoInmortal()
+    {
+        inmortal = true;
+        yield return new WaitForSeconds(tiempoInmortal);
+        inmortal = false;
+    }
+}
