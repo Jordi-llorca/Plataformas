99fbf18845adb8bd126902f0758f9ad2833c9f4d
Jordi <jorllorber@gmail.com>
2020-11-22 21:38:03 +0100

    LightsAdded

--- Assets/Scripts/camaraMovement.cs
+++ Assets/Scripts/camaraMovement.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class camaraMovement : MonoBehaviour
 {
     public GameObject jugador;
-
+    public float limiteInferior;
+    public float limiteSuperior;
     private void FixedUpdate()
     {
-        transform.position = new Vector3(jugador.transform.position.x, 0 , transform.position.z);
-        transform.position = new Vector3(0, jugador.transform.position.y, transform.position.z);
+        if (jugador.transform.position.y > limiteInferior && jugador.transform.position.y < limiteSuperior)
+            transform.position = new Vector3(0, jugador.transform.position.y, transform.position.z);
+        else if(jugador.transform.position.y < limiteInferior)
+            transform.position = new Vector3(0, limiteInferior, transform.position.z);
+        else
+            transform.position = new Vector3(0, limiteSuperior, transform.position.z);
     }
 }
