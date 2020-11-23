0af5747cddd737df3a81c9f3c4ebaf449f8c76f6
delniss <De12li13a14@gmail.com>
2020-11-23 15:46:54 +0100

    stamina bar baston

--- Assets/Scripts/baston.cs
+++ Assets/Scripts/baston.cs
@@ -9,21 +9,34 @@ public class baston : MonoBehaviour
     public Transform shotPoint;
     private float timeBtwShots;
     public float startTimeBtwShots=0.25f;
+    public barraJetPack staminaBar;
+    float stamina;
+    void Start()
+    {
+        staminaBar.SetMaxFuel(startTimeBtwShots);
+        staminaBar.SetFuel(startTimeBtwShots);
+        stamina = startTimeBtwShots;
+    }
     void Update()
     {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f,0f,rotZ + offset);
 
+        staminaBar.SetFuel(stamina);
         if(timeBtwShots<=0)
         {
             if(Input.GetMouseButtonDown(0))
             {
+                staminaBar.SetFuel(0);
                 Instantiate(projectile, shotPoint.position, transform.rotation);
                 timeBtwShots = startTimeBtwShots;
+                stamina=0;
             }
         }else{
-           timeBtwShots -=Time.deltaTime;
+        
+            timeBtwShots -=Time.deltaTime;
+            stamina+=Time.deltaTime;
         } 
     }
 }
