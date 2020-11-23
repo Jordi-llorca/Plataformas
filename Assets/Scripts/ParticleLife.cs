9fc70fc6c0ba95b94ed2072823b29ac9da932aeb
delniss <De12li13a14@gmail.com>
2020-11-23 01:45:13 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Jordi-llorca/Plataformas into main""
    This reverts commit b359f3d8bc303a034ca6e301a3ac32820a110f2e.

---
+++ Assets/Scripts/ParticleLife.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleLife : MonoBehaviour
+{
+
+    public float lifeTime;
+    float currentLife;
+    private void Start()
+    {
+        currentLife = lifeTime;
+    }
+    void Update()
+    {
+        currentLife -= Time.deltaTime;
+        if (currentLife <= 0) Destroy(gameObject);
+    }
+}
